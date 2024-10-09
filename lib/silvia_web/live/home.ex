4014b5fcd7038ac64dc7fee40144af6857db3ecd
Jeffrey S. Morgan <cheezy@industriallogic.com>
2024-10-09 14:25:48 -0400

    Removed unused targets and prettied up the ui

--- lib/silvia_web/live/home.ex
+++ lib/silvia_web/live/home.ex
@@ -11,10 +11,12 @@ defmodule SilviaWeb.Home do
     ~H"""
     <.page_title title="Rancilio Silva Dashboard" />
     <.body>
-      Coffee maching stuff will show up here!
-
+      Coffee machine stuff will show up here!
+      <br/><br/>
       This project uses Nerves for RPI hardware integration and LiveView
-      for the web interface. Click to learn more about
+      for the web interface.
+      <br/><br/>
+      Click to learn more about
       <.link_to href="https://nerves-project.org">Nerves</.link_to>
       and
       <.link_to href="https://www.phoenixframework.org/">LiveView</.link_to>.
--- mix.exs
+++ mix.exs
@@ -4,17 +4,8 @@ defmodule Silvia.MixProject do
   @app :silvia
   @version "0.1.0"
   @all_targets [
-    :rpi,
     :rpi0,
-    :rpi2,
-    :rpi3,
-    :rpi3a,
-    :rpi4,
-    :bbb,
-    :osd32mp1,
-    :x86_64,
-    :grisp2,
-    :mangopi_mq_pro
+    :rpi3
   ]
 
   def project do
@@ -89,17 +80,8 @@ defmodule Silvia.MixProject do
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.19", runtime: false, targets: :rpi},
       {:nerves_system_rpi0, "~> 1.19", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.19", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.19", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.19", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.19", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.14", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.10", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.19", runtime: false, targets: :x86_64},
-      {:nerves_system_grisp2, "~> 0.3", runtime: false, targets: :grisp2},
-      {:nerves_system_mangopi_mq_pro, "~> 0.4", runtime: false, targets: :mangopi_mq_pro}
+      {:nerves_system_rpi3, "~> 1.19", runtime: false, targets: :rpi3}
     ]
   end
 
