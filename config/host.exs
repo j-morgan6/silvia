b9c3059fb8321d460f7cab267168d21f39428cf8
Jeffrey S. Morgan <cheezy@industriallogic.com>
2024-10-16 09:48:49 -0400

    Added config so VintageNet doesn't throw errors when started on host.

--- config/host.exs
+++ config/host.exs
@@ -3,3 +3,9 @@ import Config
 # Used when running locally - running tests
 config :silvia, vintage_net: Fake.VintageNet
 config :silvia, vintage_net_wizard: Fake.VintageNetWizard
+
+# Have VintageNet not try to resolve so things can run locally
+config :vintage_net,
+  resolvconf: "/dev/null",
+  persistence: VintageNet.Persistence.Null,
+  bin_ip: "false"
