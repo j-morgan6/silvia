f0eaecc9ac70b2f3a5b1560c456d9101ad8f6811
Jeffrey S. Morgan <cheezy@industriallogic.com>
2024-10-16 21:48:15 -0400

    Added Tango org and development deployment

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,6 @@ set -e
 
 export MIX_ENV=prod
 export MIX_TARGET=rpi0
+export NERVES_HUB_ORG=Tango
 
-mix nerves_hub.user auth
-mix nerves_hub.firmware publish --key tangokey --product Silvia
+mix nerves_hub.firmware publish --key tangokey --product Silvia --deploy development
