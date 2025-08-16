663b4edb32cfc6cc5e5df7ca5be51e9762ea019d
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-08-16 16:12:11 -0400

    Updated publish script to sign and publish using the new nerves hub command line tool.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,15 @@ set -e
 export MIX_ENV=prod
 export MIX_TARGET=rpi0
 export NERVES_HUB_ORG=Tango
+export NERVES_HUB_PRODUCT=Silvia
 export NERVES_FW_AUTHOR=Tango
 
-mix nerves_hub.firmware publish --key tangokey --product Silvia --deploy development
+export HOST=silvia.local
+
+MIX_TARGET=host mix do setup, assets.deploy
+mix deps.get
+mix firmware
+
+nh firmware sign ./_build/${MIX_TARGET}_${MIX_ENV}/nerves/images/silvia.fw --key tangokey
+
+nh firmware publish ./_build/${MIX_TARGET}_${MIX_ENV}/nerves/images/silvia.fw --deploy development
